014643311df72484add18e7d633008193ec2d5c2
JBGelsenorJ <javi_belda@live.com>
2019-10-31 13:45:16 +0100

    Mouse click

--- Physics2_class4 - handout/ModulePhysics.cpp
+++ Physics2_class4 - handout/ModulePhysics.cpp
@@ -31,9 +31,9 @@ bool ModulePhysics::Start()
 	world = new b2World(b2Vec2(GRAVITY_X, -GRAVITY_Y));
 	world->SetContactListener(this);
 
-	//// needed to create joints like mouse joint
-	//b2BodyDef bd;
-	//ground = world->CreateBody(&bd);
+	 //needed to create joints like mouse joint
+	b2BodyDef bd;
+	ground = world->CreateBody(&bd);
 
 	//// big static circle as "ground" in the middle of the screen
 	//int x = SCREEN_WIDTH / 2;
